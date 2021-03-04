552b28595c311a23086b22b622d1f67944b8fb9a
pingfan108 <tuobaye2006@gmail.com>
2021-03-05 00:33:02 +0800

    [comment] add comments for util/arena

--- util/arena.cc
+++ util/arena.cc
@@ -12,6 +12,7 @@ Arena::Arena()
     : alloc_ptr_(nullptr), alloc_bytes_remaining_(0), memory_usage_(0) {}
 
 Arena::~Arena() {
+  // 把所有申请的block释放
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
   }
@@ -19,12 +20,14 @@ Arena::~Arena() {
 
 char* Arena::AllocateFallback(size_t bytes) {
   if (bytes > kBlockSize / 4) {
+    // 用户需要的内存足够大, 直接按需申请分配即可
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
     char* result = AllocateNewBlock(bytes);
     return result;
   }
 
+  // 用户需要的内存比较小, 容易产生碎片; 统一按4k申请, 按需切割后分配
   // We waste the remaining space in the current block.
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
@@ -36,11 +39,15 @@ char* Arena::AllocateFallback(size_t bytes) {
 }
 
 char* Arena::AllocateAligned(size_t bytes) {
+  // 检查机器的位数, 高于64位则以机器位数折算的字节数对齐; 反之, 最低以8字节对齐
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   static_assert((align & (align - 1)) == 0,
                 "Pointer size should be a power of 2");
+  // a&(n-1), 等价于a%n, 取得当前地址超出对齐部分的字节数
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
+  // 计算要令当前地址对齐所需填充的字节数
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
+  // 实际所需的空间需要在用户所申请大小的基础上增加填充的字节数
   size_t needed = bytes + slop;
   char* result;
   if (needed <= alloc_bytes_remaining_) {
@@ -51,6 +58,7 @@ char* Arena::AllocateAligned(size_t bytes) {
     // AllocateFallback always returned aligned memory
     result = AllocateFallback(bytes);
   }
+  // 验证最终分配的地址是按align字节数对齐的
   assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0);
   return result;
 }
--- util/arena.h
+++ util/arena.h
@@ -17,6 +17,7 @@ class Arena {
  public:
   Arena();
 
+  // 禁用拷贝构造函数和赋值运算符 ==> 单例
   Arena(const Arena&) = delete;
   Arena& operator=(const Arena&) = delete;
 
@@ -35,15 +36,15 @@ class Arena {
   }
 
  private:
-  char* AllocateFallback(size_t bytes);
-  char* AllocateNewBlock(size_t block_bytes);
+  char* AllocateFallback(size_t bytes);        // 分配失败处理
+  char* AllocateNewBlock(size_t block_bytes);  // 分配新的block
 
-  // Allocation state
-  char* alloc_ptr_;
-  size_t alloc_bytes_remaining_;
+  // Allocation state(当前block的分配状态)
+  char* alloc_ptr_;               // 指向当前block未分配空间首字节
+  size_t alloc_bytes_remaining_;  // 当前block剩余未分配字节数
 
   // Array of new[] allocated memory blocks
-  std::vector<char*> blocks_;
+  std::vector<char*> blocks_;     // 所有已申请(且都分配了部分)的block
 
   // Total memory usage of the arena.
   //
@@ -57,12 +58,14 @@ inline char* Arena::Allocate(size_t bytes) {
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
   assert(bytes > 0);
+  // 如果当前block还有剩余空间, 直接从当前block分配
   if (bytes <= alloc_bytes_remaining_) {
     char* result = alloc_ptr_;
     alloc_ptr_ += bytes;
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
+  // 转分配失败处理
   return AllocateFallback(bytes);
 }
 
